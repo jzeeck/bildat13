0c3c3dae037f7aae547ba40835914358edc6d919
Viktor Collin <viktor.collin@gmail.com>
2013-11-29 08:37:49 +0100

    lab 1

--- histogram6.m
+++ histogram6.m
@@ -1,8 +1,15 @@
-% ret = histeq(nallo256,16,1)
-% figure(3);
-% showgray(ret);
-
-what = phonecalc256;
-ret = histeq(what,256,1)
-figure(3);
+ret = histeq(nallo256,256,1)
+figure('name','histeg nallo');
 showgray(ret);
+
+%{
+ret = histeq(nallo256what,256,1)
+figure('name','histeg nallowhat');
+showgray(ret);
+figure('name','3th root of nallow what');
+showgray(nallo256what.^(1/3));
+%}
+%what = phonecalc256;
+%ret = histeq(what,256,1)
+%figure('name','histeg phone');
+%showgray(ret);
