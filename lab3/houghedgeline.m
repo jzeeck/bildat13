1801603b209dacb258e4191bf79d59eb26a3ae07
Johan Zeeck <jzeeck@kth.se>
2013-12-16 14:59:02 +0100

    think it is working now

--- lab3/houghedgeline.m
+++ lab3/houghedgeline.m
@@ -3,9 +3,19 @@ function [ linepar, acc ] = houghedgeline( pic, scale, gradmagnthreshold, ...
 
                                         
 curves  = njetedge(pic, scale, gradmagnthreshold);
+gradient = Lv(pic);
+
+%remove all the index elements
+temp = [];
+for i =1:size(curves, 2)
+    if curves(1,i) ~= 0
+        temp = [temp curves(:,i)];
+    end
+end
+
                                         
-[ linepar, acc ] = houghline( curves, gradmagnthreshold, ...
-                 nrho, ntheta, threshold, nlines, verbose );
+[ linepar, acc ] = houghline( temp, gradient, ...
+                 nrho, ntheta, gradmagnthreshold, nlines, verbose );
 
 
 end
--- lab3/test_houghline.m
+++ lab3/test_houghline.m
@@ -1,30 +1,70 @@
+%%
 close all;
 clc;
-curves = [40 57 74
-          70 60 50];
-magnitude = 4711;
-nrho = 10;
-ntheta = 6;
-threshold = 4711;
-nlines = 2;
+% curves = [50 1 50 45
+%           1 50 25 45];
+% magnitude = 4711;
+nrho = 300;
+ntheta = 80;
+threshold = 0.01;
+nlines = 4;
 verbose = 0;
+% 
+% 
+% 
+% for i = 1:size(curves, 2)
+%     tempPic(curves(1,i),curves(2,i)) = 0;
+% end
 
+dim = 256;
+tempPic = zeros(dim);
 
-tempPic = ones(128);
-for i = 1:size(curves, 2)
-    tempPic(curves(1,i),curves(2,i)) = 0;
+for i = 1:dim
+    for j = 1:dim
+       if i > 50 && i < 200
+           if i + j >= 150 && i + j <= 175 
+               tempPic(i,j) = 1;
+           end
+       end
+    end
+end
+
+for i = 1:dim
+    for j = 1:dim
+       if i > 50 && i < 200
+           if i >= 150 && i <= 175 
+               tempPic(i,j) = 1;
+           end
+       end
+    end
 end
 figure;
+subplot(1,2,1)
 showgray(tempPic)
+axis on;
 
-houghline(curves,magnitude,nrho,ntheta,threshold,nlines, verbose);
 
+subplot(1,2,2)
+curves  = njetedge(tempPic,1.0,0.01);
+overlaycurves(tempPic, curves);
+
+
+houghedgeline(tempPic,1.0,threshold,nrho,ntheta,nlines, verbose);
+
+%%
 
 figure;
-t = triangle128;
-showgrey(t)
-c = zerocrosscurves(t-128);
-overlaycurves(t, c)
-figure;
-showgrey(pixelplotcurves(t, c, -128));
-
+test = houghtest256;
+subplot(1,2,1)
+showgrey(test)
+subplot(1,2,2)
+houghedgeline(test,1.0,threshold,nrho,ntheta,10, verbose);
+% 
+% figure;
+% t = triangle128;
+% showgrey(t)
+% c = zerocrosscurves(t-128);
+% overlaycurves(t, c)
+% figure;
+% showgrey(pixelplotcurves(t, c, -128));
+% 
